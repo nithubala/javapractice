e2583e5c1bc1eee6be9d3a2e024f9fd9018ba25b
nithubala <nithubala96@hmail.com>
2019-08-14 01:30:01 -0400

    Validation is done

---
+++ src/main/java/com/revature/Validation.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class Validation {
+	public static void method(String name) {
+		if(name.equals("Nithya")) {
+			System.out.println("Valid name");
+		}
+		else {
+			System.out.println("Invalid name");
+		}
+	}
+
+}
---
+++ src/test/java/com/revature/TestValidation.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class TestValidation {
+public static void main(String[] args) {
+	String name="Nithya";
+	Validation.method(name);
+}
+}
